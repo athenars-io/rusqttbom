dd2b6f801ee36f6ea290155671850dd306cc9999
adenoz <deno@adenoz.com>
2023-01-07 20:14:42 +1100

    sorting start of project

---
+++ target/debug/deps/rusqttbom-013dcfc04006f717.d
@@ -0,0 +1,5 @@
+/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-013dcfc04006f717.rmeta: src/main.rs
+
+/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-013dcfc04006f717.d: src/main.rs
+
+src/main.rs:
