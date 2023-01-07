678ea96e68bd3edb5762dd143320e02d1d6a3b2d
adenoz <deno@adenoz.com>
2023-01-07 20:19:55 +1100

    deleted target directory
    I forgot to place it in the .gitignore first

--- target/debug/deps/rusqttbom-013dcfc04006f717.d
+++
@@ -1,5 +0,0 @@
-/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-013dcfc04006f717.rmeta: src/main.rs
-
-/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-013dcfc04006f717.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/rusqttbom-05d72c5032ec636d.d
+++
@@ -1,5 +0,0 @@
-/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-05d72c5032ec636d.rmeta: src/main.rs
-
-/Users/adam/Nextcloud/Documents/programming/athenars/rusqttbom/target/debug/deps/rusqttbom-05d72c5032ec636d.d: src/main.rs
-
-src/main.rs:
