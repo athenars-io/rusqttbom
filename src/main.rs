2732ce7085cd3c6b2bb2cfd6a6093208a7a21e73
adenoz <deno@adenoz.com>
2023-01-09 16:05:09 +1100

    data is now parsed and saved as variables

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use reqwest::{self, header::CONTENT_TYPE};
 use std::error::Error;
-//use std::io::Read;
 use serde::{Deserialize, Serialize};
 //use serde_json;
-//use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct APIData {
@@ -15,14 +13,30 @@ struct APIData {
 struct Weather {
     temp: f32,
     temp_feels_like: f32,
+    min_temp: MinTemp,
+    max_temp: MaxTemp,
     humidity: u32,
     rain_since_9am: f32,
-    wind: Wind
+    // gust: String, // how to manage null values?
+    // max_gust: String // how to manage null values?
+    wind: Wind,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Wind {
-    speed_knot: u32,
+    speed_knot: f32,
+    speed_kilometre: f32,
+    direction: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MaxTemp {
+    value: f32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MinTemp {
+    value: f32,
 }
 
 async fn get_weather() -> Result<APIData, Box<dyn Error>> {
@@ -30,19 +44,27 @@ async fn get_weather() -> Result<APIData, Box<dyn Error>> {
     let response = client
         .get("https://api.weather.bom.gov.au/v1/locations/r3gx2f/observations")
         .header(CONTENT_TYPE, "application/json")
-        //.header(ACCEPT, "application/json")
         .send()
         .await?
         .json::<APIData>()
         .await?;
-        //.unwrap();
 
-    // The below works!!!!!!!!!!!
+    // the following variables can later be published via
+    // MQTT messages along their own MQTT topics.
+    // At that point, we will no longer need the println! lines
+    // as those lines are temporary only.
+
     let current_temp = &response.data.temp;
     println!("The current temperature is {:?} degrees", &current_temp);
 
     let temp_feels = &response.data.temp_feels_like;
     println!("The current temperature feels like {:?} degrees", temp_feels);
+
+    let min_temp = &response.data.min_temp.value;
+    println!("The minimum temperature today has been {:?} degrees", min_temp);
+
+    let max_temp = &response.data.max_temp.value;
+    println!("The maximum temperature today has been {:?} degrees", max_temp);
 
     let humidity = &response.data.humidity;
     println!("The current humidty is {:?} percent", humidity);
@@ -50,36 +72,21 @@ async fn get_weather() -> Result<APIData, Box<dyn Error>> {
     let rain_today = &response.data.rain_since_9am;
     println!("Today, there has been {:?}mm of rain", rain_today);
 
+    let wind_kms = &response.data.wind.speed_kilometre;
+    println!("The wind speed is {:?}km/h", wind_kms);
 
-       
-    //println!("Got {:?}", &response);
-    //println!("response status = {}", &response.status());
+    let wind_kts = &response.data.wind.speed_knot;
+    println!("the wind speed is {:?}knots", wind_kts);
 
-    //let body = &response.text().await;
-    //println!("Body {:?}", &body);
-
-    //let tt = json::parse(&body).unwrap();
-
-    //let v: Value = serde_json::from_string(&body).unwrap();
+    let wind_direction = &response.data.wind.direction;
+    println!("The wind is coming from the {:?} direction", wind_direction);
 
     // The below works well and correct
+    // We can build on the below later for error checking etc.
     //match &response.json::<APIData>().await {
     //    Ok(parsed) => println!("Success! {:?}", parsed),
     //    Err(_) => println!("didn't parse"),
     //};
-
-    //println!("issue time is {}", &parsed["issue_time"])
-
-    //let metaz = &response.json::<APIData>().await;
-
-    //for line in metaz {
-    //    println!("{:#?}", line);
-    //    println!("test {}", line.metadata)
-    //}
-
-    //let textr = &response.text().await;
-
-    //println!("to text {:?}", textr);
 
     Ok(response)
 }
