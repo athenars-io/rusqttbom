a4e911c3db440d5c087c0e5476bc581236a2106e
adenoz <deno@adenoz.com>
2023-01-19 14:04:08 +1100

    now have async mqtt message sending

--- src/observations.rs
+++ src/observations.rs
@@ -6,8 +6,8 @@ use std::time::Duration;
 use std::{error::Error, fs};
 use toml;
 // use std::thread;
-// use tokio::{task, time};
-// use serde_json;
+use tokio::task; //time
+                 // use serde_json;
 
 #[derive(Deserialize, Debug)]
 struct Config {
@@ -73,23 +73,11 @@ struct MinTemp {
     value: Option<f32>,
 }
 
-// impl Weather {
-//     pub fn send_temp(&self) {
-//         println!("the temp is {:?}", self.temp)
-//         // send_mqtt("rusqttbom/temp", self.temp)
-//     }
-// }
-
 impl APIData {
     // Methods for returning weather data, if it exists. Returns Some or None.
     fn get_temp(&self) -> Option<f32> {
-        // println!("self temp is {:?}", self.data.temp);
         self.data.temp
     }
-
-    // fn valid_temp(&self) -> bool {
-    //     Some(self.data.temp) < 50: f32;
-    // }
 
     fn get_temp_feels(&self) -> Option<f32> {
         self.data.temp_feels_like
@@ -110,15 +98,6 @@ impl APIData {
     fn get_rain(&self) -> Option<f32> {
         self.data.rain_since_9am
     }
-
-    // fn get_wind_km(&self) -> Option<f32> {
-    //     self.data.wind?.speed_kilometre
-    // }
-
-    // Will need to deal with wind direction later, as cannot copy, clone strings
-    // fn get_wind_dir(&self) -> Option<String> {
-    //     self.data.wind?.direction
-    // }
 
     fn get_gusts(&self) -> Option<f32> {
         self.data.gust?.speed_kilometre
@@ -142,13 +121,17 @@ async fn send_mqtt(topicz: &str, payloadz: String) -> Result<(), Box<dyn Error>>
     mqttoptions.set_keep_alive(Duration::from_secs(5));
     let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
 
-    client
-        .publish(topicz, QoS::AtMostOnce, false, payloadz)
-        .await?;
+    let topiczz = topicz.to_string();
+    task::spawn(async move {
+        sendit(client.clone(), topiczz, payloadz).await;
+        client.disconnect().await.expect("Could not disconnect");
+        // time::sleep(Duration::from_secs(3)).await;
+    });
 
     // This endless eventloop is required to publish the messages
-    // The count needs to give enough times to receive the ConnAck and complete this program and wait for any failed messages to process
-    let mut n = 15;
+    // The count gives enough time to deal with connection issues,
+    // however, each task should disconnect deliberately after publishing
+    let mut n = 5;
     loop {
         let event = eventloop.poll().await;
         n -= 1; // this countdown allows us to break out of the endless loop closing the program
@@ -168,11 +151,29 @@ async fn send_mqtt(topicz: &str, payloadz: String) -> Result<(), Box<dyn Error>>
     }
 }
 
+async fn sendit(client: AsyncClient, topicz: String, payloadz: String) {
+    client
+        .publish(topicz, QoS::AtMostOnce, false, payloadz)
+        .await
+        .unwrap();
+}
+
 fn valid_temp(temp: f32) -> bool {
     temp > -20.0 && temp < 50.0
 }
 
-// Result<(), Box<dyn Error>>
+fn valid_wind(wind: f32) -> bool {
+    wind > -1.0 && wind < 400.0
+}
+
+fn valid_humidity(humidity: f32) -> bool {
+    humidity > -1.0 && humidity < 101.0
+}
+
+fn valid_rain(rain: f32) -> bool {
+    rain > -1.0 && rain < 300.0
+}
+
 pub async fn get_observations() -> Result<(), Box<dyn Error>> {
     // Optionally, you may want to replace `crate::get_config_path()` with a custom hard coded file path for development
     // Regardless, setting the config file path allows for the binary to run, including by CRON
@@ -186,155 +187,104 @@ pub async fn get_observations() -> Result<(), Box<dyn Error>> {
 
     let url = format!("https://api.weather.bom.gov.au/v1/locations/{loc_hash}/observations");
 
-    // GET the BOM API data
+    // GET the BOM API data and send to the structs
     let client = reqwest::Client::new();
     let response = client
         .get(url)
         .header(CONTENT_TYPE, "application/json")
         .send()
         .await?
-        // .unwrap()
-        .json::<APIData>()
+        .json::<APIData>() // this is where the data is sent to the top level struct
         .await?;
 
-    // send_mqtt("rusqtttest", "24.5").await;
-    // let mut f = Weather {};
-    // f.send_temp();
-
-    // response.get_temp();
-
-    //#################################################
-
-    // // Setup MQTT client to be ready to send messages
-    // let ip = config.broker.ip;
-    // let port = config.broker.port;
-    // let mut mqttoptions = MqttOptions::new("rusqttbom", ip, port);
-    // mqttoptions.set_keep_alive(Duration::from_secs(5));
-    // let (client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
-
     // Publish the data as MQTT messages
-    // Calling the methods which return Some or None
-    let temp_c_topic = "rusqtttest/weather/current-temp";
-    let mut temp_string = String::new();
     if let Some(temppp) = response.get_temp() {
         if valid_temp(temppp) {
+            let mut temp_string = String::new();
             temp_string = temppp.to_string();
+            let temp_c_topic = "outside/weather/current-temp";
+            send_mqtt(temp_c_topic, temp_string).await?;
         }
     }
-    send_mqtt(temp_c_topic, temp_string).await?;
 
+    if let Some(temp_feels) = response.get_temp_feels() {
+        if valid_temp(temp_feels) {
+            let mut temp_feels_string = String::new();
+            temp_feels_string = temp_feels.to_string();
+            let temp_feels_topic = "outside/weather/temp-feels";
+            send_mqtt(temp_feels_topic, temp_feels_string).await?;
+        }
+    }
+
+    if let Some(min_temppp) = response.get_min_temp() {
+        if valid_temp(min_temppp) {
+            let mut min_temp_string = String::new();
+            min_temp_string = min_temppp.to_string();
+            let min_temp_topic = "outside/weather/min-temp";
+            send_mqtt(min_temp_topic, min_temp_string).await?;
+        }
+    }
+
+    if let Some(max_temppp) = response.get_max_temp() {
+        if valid_temp(max_temppp) {
+            let mut max_temp_string = String::new();
+            max_temp_string = max_temppp.to_string();
+            let max_temp_topic = "outside/weather/max-temp";
+            send_mqtt(max_temp_topic, max_temp_string).await?;
+        }
+    }
+
+    if let Some(humidityyy) = response.get_humidity() {
+        if valid_humidity(humidityyy) {
+            let mut humidity_string = String::new();
+            humidity_string = humidityyy.to_string();
+            let humidity_topic = "outside/weather/humidity";
+            send_mqtt(humidity_topic, humidity_string).await?;
+        }
+    }
+
+    if let Some(rainnn) = response.get_rain() {
+        if valid_rain(rainnn) {
+            let mut rain_string = String::new();
+            rain_string = rainnn.to_string();
+            let rain_today_topic = "outside/weather/rain-today";
+            send_mqtt(rain_today_topic, rain_string).await?;
+        }
+    }
+
+    // // Note wind kms managed differently
+    if let Some(windkm) = response.data.wind.speed_kilometre {
+        if valid_wind(windkm) {
+            let mut windstring = String::new();
+            windstring = windkm.to_string();
+            let wind_km_topic = "outside/weather/wind-kms";
+            send_mqtt(wind_km_topic, windstring).await?;
+        }
+    }
+
+    if let Some(winddir) = &response.data.wind.direction {
+        let mut wind_dir_string = String::new();
+        wind_dir_string = winddir.to_string();
+        let wind_dir_topic = "outside/weather/wind-dir";
+        send_mqtt(wind_dir_topic, wind_dir_string).await?;
+    }
+
+    if let Some(guggg) = response.get_gusts() {
+        if valid_wind(guggg) {
+            let mut gust_string = String::new();
+            gust_string = guggg.to_string();
+            let gusts_topic = "outside/weather/gusts-kms";
+            send_mqtt(gusts_topic, gust_string).await.unwrap();
+        }
+    }
+
+    if let Some(maxw) = response.get_gusts_max() {
+        if valid_wind(maxw) {
+            let mut max_wind_string = String::new();
+            max_wind_string = maxw.to_string();
+            let max_gust_topic = "outside/weather/max-gust";
+            send_mqtt(max_gust_topic, max_wind_string).await.unwrap();
+        }
+    }
     Ok(())
-    // client
-    //     .publish(temp_c_topic, QoS::AtMostOnce, false, temp_string)
-    //     .await?;
-
-    // let temp_feels_topic = "outside/weather/temp-feels";
-    // let mut temp_feels_string = String::new();
-    // if let Some(temp_feels) = response.get_temp_feels() {
-    //     temp_feels_string = temp_feels.to_string();
-    // }
-    // client
-    //     .publish(temp_feels_topic, QoS::AtMostOnce, false, temp_feels_string)
-    //     .await?;
-
-    // let min_temp_topic = "outside/weather/min-temp";
-    // let mut min_temp_string = String::new();
-    // if let Some(min_temppp) = response.get_min_temp() {
-    //     min_temp_string = min_temppp.to_string();
-    // }
-    // client
-    //     .publish(min_temp_topic, QoS::AtMostOnce, false, min_temp_string)
-    //     .await?;
-
-    // let max_temp_topic = "outside/weather/max-temp";
-    // let mut max_temp_string = String::new();
-    // if let Some(max_temppp) = response.get_max_temp() {
-    //     max_temp_string = max_temppp.to_string();
-    // }
-    // client
-    //     .publish(max_temp_topic, QoS::AtMostOnce, false, max_temp_string)
-    //     .await?;
-
-    // let humidity_topic = "outside/weather/humidity";
-    // let mut humidity_string = String::new();
-    // if let Some(humidityyy) = response.get_humidity() {
-    //     humidity_string = humidityyy.to_string();
-    // }
-    // client
-    //     .publish(humidity_topic, QoS::AtMostOnce, false, humidity_string)
-    //     .await?;
-
-    // let rain_today_topic = "outside/weather/rain-today";
-    // let mut rain_string = String::new();
-    // if let Some(rainnn) = response.get_rain() {
-    //     rain_string = rainnn.to_string();
-    // }
-    // client
-    //     .publish(rain_today_topic, QoS::AtMostOnce, false, rain_string)
-    //     .await?;
-
-    // let wind_km_topic = "outside/weather/wind-kms";
-    // let mut windstring = String::new();
-    // if let Some(windkm) = &response.data.wind.speed_kilometre {
-    //     windstring = windkm.to_string();
-    // }
-    // client
-    //     .publish(wind_km_topic, QoS::AtMostOnce, false, windstring)
-    //     .await?;
-
-    // let wind_dir_topic = "outside/weather/wind-dir";
-    // let mut wind_dir_string = String::new();
-    // if let Some(winddir) = &response.data.wind.direction {
-    //     wind_dir_string = winddir.to_string();
-    // }
-    // client
-    //     .publish(wind_dir_topic, QoS::AtMostOnce, false, wind_dir_string)
-    //     .await?;
-
-    // let gusts_topic = "outside/weather/gusts-kms";
-    // let mut gust_string = String::new();
-    // if let Some(guggg) = response.get_gusts() {
-    //     gust_string = guggg.to_string();
-    // }
-    // client
-    //     .publish(gusts_topic, QoS::AtMostOnce, false, gust_string)
-    //     .await?;
-
-    // let max_gust_topic = "outside/weather/max-gust";
-    // let mut max_wind_string = String::new();
-    // if let Some(maxw) = response.get_gusts_max() {
-    //     max_wind_string = maxw.to_string();
-    // }
-    // client
-    //     .publish(max_gust_topic, QoS::AtMostOnce, false, max_wind_string)
-    //     .await?;
-
-    // This endless eventloop is required to publish the messages
-    // The count needs to give enough times to receive the ConnAck and complete this program and wait for any failed messages to process
-    // let mut n = 15;
-    // loop {
-    //     let event = eventloop.poll().await;
-    //     n -= 1; // this countdown allows us to break out of the endless loop closing the program
-    //     match &event {
-    //         Ok(v) => {
-    //             if n < 1 {
-    //                 println!("Breaking out. Program now closing");
-    //                 return Ok(());
-    //             }
-    //             println!("Event = {:?}", v);
-    //         }
-    //         Err(e) => {
-    //             println!("Error = {:?}", e);
-    //             return Ok(());
-    //         }
-    //     }
-    // }
-
-    // The below works well and correct
-    // We can build on the below later for error checking etc.
-    //match &response.json::<APIData>().await {
-    //    Ok(parsed) => println!("Success! {:?}", parsed),
-    //    Err(_) => println!("didn't parse"),
-    //};
-    // Ok(response)
 }
